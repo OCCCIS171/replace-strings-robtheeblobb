201d69aefd5e7ccfe99568e9e1db9e176ea3b52e
Robert Simpson <159501264+robtheeblobb@users.noreply.github.com>
2024-02-11 09:26:41 -0800

    Update replace-string.js
    Signed-off-by: Robert Simpson <159501264+robtheeblobb@users.noreply.github.com>

--- replace-string.js
+++ replace-string.js
@@ -1,9 +1,13 @@
 let myString = 'Hello World!!!';
 
+console.log(myString)
+
 // TODO: replace the first occurence of 'o' with '0' in `myString` defined above.
 // NOTE: DO NOT HARD CODE THE SIZE OF `myString`. When you change the value
 // of `myString`, the test should still pass.
-let newString = myString.replace("o" , "0";
+let newString = myString.replace("o" , "0");
+
+console.log(newString)
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 
